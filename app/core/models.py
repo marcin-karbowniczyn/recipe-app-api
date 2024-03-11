934eb4846b58cecf42392b7175d7449e543c40e9
Marcin <marcin.karbowniczyn@gmail.com>
2024-03-11 18:37:21 +0100

    Add create_superuser function, add tests for creating superuser

--- app/core/models.py
+++ app/core/models.py
@@ -7,14 +7,21 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """ Create, save and return a new user """
 
-        # 1. Create user and normalize the email (set the domain part to lowercase)
+        if not email:
+            raise ValueError('User must have an email address.')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
-
-        # 2. Hash the password
         user.set_password(password)
-
-        # 3. Save and return the user
         user.save(using=self._db)  # We set the "using" parameter to support adding multiple DB's.
+        return user
+
+    def create_superuser(self, email, password):
+        """ Create and return a new superuser """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
         return user
 
 
