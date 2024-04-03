d6207416162988bc98bc4b2eb2d25d35d31dc5b0
Marcin <marcin.karbowniczyn@gmail.com>
2024-04-03 18:44:41 +0200

    Add custom update method to handle many-to-many fields in recipe serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -27,28 +27,41 @@ class RecipeSerializer(serializers.ModelSerializer):
     #     response['user'] = instance.user.name
     #     return response
 
+    def _get_or_create_tags(self, tags, recipe):
+        """ Handle getting or creating tags """
+        authenticated_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(user=authenticated_user, **tag)
+            recipe.tags.add(tag_obj)
+
+    # We need to specify this because nested fields are read-only by default.
+    # We need to override create() and update() methods for the creating and updating to work.
     def create(self, validated_data):
         """ Create a recipe """
-        # 1. Remove tags from validated data
+        # 1. Remove tags from validated data and return them
         tags = validated_data.pop('tags', [])
 
         # 2. Create a new recipe object with correct values
         recipe = Recipe.objects.create(**validated_data)
 
-        # 3. Get authenticated user from the request
-        auth_user = self.context['request'].user
-
-        # 4. Loop through tags from validated data
-        for tag in tags:
-            # get_or_create() -> Manager's helper function, it gets the object if it exists, if not it creates it.
-            tag_obj, created = Tag.objects.get_or_create(
-                user=auth_user,
-                **tag,
-            )
-            # add() -> Adds object to many-to-many field of related object (Recipe in our case)
-            recipe.tags.add(tag_obj)
+        # 3. Get authenticated user from the request and loop through tags from validated data
+        self._get_or_create_tags(tags, recipe)
 
         return recipe
+
+    def update(self, instance, validated_data):
+        """ Update a recipe """
+        tags = validated_data.pop('tags', None)
+        # If empty array, it will enter the if block and clear all the tags from the recipe
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)  # Instance = recipe
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
