f80f1a576768386ca4dd9950d2da262be2208b56
Marcin <marcin.karbowniczyn@gmail.com>
2024-05-14 14:22:58 +0200

    Update setting.py, fix a typo in docker-compose-deploy.yml, update docker-compose.yml with DEBUG=1

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0r-h3^dee6xthu(!#34x4lp_v4i8*5gbypn6wyw9k!4a%^g@ei'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(None, os.getenv('ALLOWED_HOSTS', '').split(','))
+)
 
 # Application definition
 
